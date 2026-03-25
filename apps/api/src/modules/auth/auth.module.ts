e48d34938075db58b3f33ca02100663048b70b56
FrostYOON <yys1423@naver.com>
2026-03-24 22:20:13 -0400

    refactor(auth): use passport jwt strategy

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AppConfigService } from '../../core/config/app-config.service';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AccessTokenGuard } from './guards/access-token.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { AccessTokenStrategy } from './strategies/access-token.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       inject: [AppConfigService],
       useFactory: (appConfig: AppConfigService) => ({
@@ -20,6 +23,7 @@ import { AccessTokenGuard } from './guards/access-token.guard';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, AccessTokenGuard],
+  providers: [AuthService, JwtAuthGuard, AccessTokenStrategy],
+  exports: [PassportModule, JwtModule, JwtAuthGuard],
 })
 export class AuthModule {}
